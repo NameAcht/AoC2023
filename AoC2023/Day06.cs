5eb74827730eb66c5bd3625135fd8ea2a0e952a5
NameAcht <appeljohannes21@gmail.com>
2023-12-10 23:01:34 +0100

    s

---
+++ AoC2023/Day06.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+
+namespace AoC2023
+{
+    internal class Day06
+    {
+        public static int Part1(string[] input)
+        {
+            var listTime = input[0].Split(' ').ToList().FindAll(num => int.TryParse(num, out int temp));
+            var listDistances = input[1].Split(' ').ToList().FindAll(num => int.TryParse(num, out int temp));
+            int wins = 0;
+            int result = 1;
+
+            for (int i = 0; i < listTime.Count; i++)
+            {
+                int timeVal = int.Parse(listTime[i]);
+                int distVal = int.Parse(listDistances[i]);
+                for (int hold = 1; hold < timeVal; hold++)
+                    if(hold * (timeVal - hold) > distVal)
+                        wins++;
+                result *= wins;
+                wins = 0;
+            }
+
+            return result;
+        }
+        public static int Part2(string[] input)
+        {
+            string time = "";
+            input[0].Split(' ').ToList().FindAll(num => int.TryParse(num, out int temp)).ForEach(num => time += num);
+            string distance = "";
+            input[1].Split(' ').ToList().FindAll(num => int.TryParse(num, out int temp)).ForEach(num => distance += num);
+
+            long timeVal = long.Parse(time);
+            long distVal = long.Parse(distance);
+
+            int wins = 0;
+
+            for (int hold = 1; hold < timeVal; hold++)
+                if (hold * (timeVal - hold) > distVal)
+                    wins++;
+
+            return wins;
+        }
+    }
+}
---
+++ AoC2023/Day07.cs
@@ -0,0 +1,18 @@
+﻿namespace AoC2023
+{
+    internal class Day07
+    {
+        public static int Part1(string[] input)
+        {
+
+
+            return 0;
+        }
+        public static int Part2(string[] input)
+        {
+
+
+            return 0;
+        }
+    }
+}
