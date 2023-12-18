73a1931e5e28fe83e6358937443519c0ba1b9015
NameAcht <appeljohannes21@gmail.com>
2023-12-18 11:32:49 +0100

    day 12 part 1

--- AoC2023/Day12.cs
+++ AoC2023/Day12.cs
@@ -1,7 +1,40 @@
-﻿namespace AoC2023
+﻿using System.Text;
+
+namespace AoC2023
 {
     internal class Day12
     {
+        public static List<int> GetBitGroups(int n)
+        {
+            var list = new List<int>();
+            string debug = Convert.ToString(n, 2);
+
+            int groupCount = 0;
+            while (n > 0)
+            {
+                if ((n & 1) == 1)
+                    groupCount++;
+                else if(groupCount != 0)
+                {
+                    list.Add(groupCount);
+                    groupCount = 0;
+                }
+                n >>= 1;
+            }
+
+            list.Add(groupCount);
+            return list;
+        }
+        public static int GetBitCount(int n)
+        {
+            int bitCount = 0;
+            while (n > 0)
+            {
+                bitCount++;
+                n = n & (n - 1);
+            }
+            return bitCount;
+        }
         public static long NextPerm(int x)
         {
             int t = (x | (x - 1)) + 1;
@@ -19,24 +52,19 @@
             int sum = 0;
             foreach (var line in input)
             {
-                var mutLine = line.ToCharArray();
-                
-                var cfg = "";
+                var cfg = new List<int>();
                 foreach(var c in line.Split(' ')[1].Split(','))
-                    cfg += c;
-                int groups = int.Parse(cfg);
+                    cfg.Add(int.Parse(c.ToString()));
+
                 string springs = line.Split(' ')[0];
 
-                int amount = groups.ToString().Sum(c => c - '0');
+                int amount = cfg.Sum();
                 int toAdd = amount - springs.Where(c => c == '#').Count();
-
                 
                 int knownMask = 0;
                 int unknownMask = 0;
-                int brokeMask = 0;
+                int springMask = 0;
                 int funcMask = 0;
-
-                var unknownIndices = new List<int>();
 
                 for (int i = 0; i < springs.Length; i++)
                 {
@@ -44,22 +72,33 @@
                         knownMask += 1 << i;
 
                     if (springs[i] == '#')
-                        brokeMask += 1 << i;
+                        springMask += 1 << i;
 
                     if (springs[i] == '.')
                         funcMask += 1 << i;
 
                     if (springs[i] == '?')
-                    {
                         unknownMask += 1 << i;
-                        unknownIndices.Add(i);
-                    }
                 }
 
+                int max = springMask | unknownMask;
+                for (int i = springMask; i <= max; i++)
+                {
+                    if ((i & funcMask) > 0)
+                        continue;
 
-                int max = FindLargestBit(unknownMask) - 1;
+                    if ((i & springMask) < springMask)
+                        continue;
 
+                    if (GetBitCount(i ^ springMask) != toAdd)
+                        continue;
 
+                    var bitGroups = GetBitGroups(i);
+                    if (!GetBitGroups(i).SequenceEqual(cfg))
+                        continue;
+
+                    sum++;
+                }
             }
 
             return sum;
