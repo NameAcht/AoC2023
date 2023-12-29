3e7902b0002a66636e6d6c82378118ba0136bcf4
NameAcht <appeljohannes21@gmail.com>
2023-12-29 23:05:39 +0100

    day 20 indentation test

--- AoC2023/Day20.cs
+++ AoC2023/Day20.cs
@@ -4,56 +4,56 @@ namespace AoC2023
 {
 	internal class Day20
 	{
-        public static long LeastCommonMultiple(long[] numbers)
-        {
-            return numbers.Aggregate(lcm);
-        }
-        static long lcm(long a, long b)
-        {
-            return Math.Abs(a * b) / GreatestCommonDivisor(a, b);
-        }
-        static long GreatestCommonDivisor(long a, long b)
-        {
-            return b == 0 ? a : GreatestCommonDivisor(b, a % b);
-        }
-        public class Module
+		public static long LeastCommonMultiple(long[] numbers)
+		{
+			return numbers.Aggregate(lcm);
+		}
+		static long lcm(long a, long b)
+		{
+			return Math.Abs(a * b) / GreatestCommonDivisor(a, b);
+		}
+		static long GreatestCommonDivisor(long a, long b)
+		{
+			return b == 0 ? a : GreatestCommonDivisor(b, a % b);
+		}
+		public class Module
 		{
 			public string name;
 			public char type;
 			public List<string> output;
-            public object state;
-            public Module(string name, char type, List<string> output)
+			public object state;
+			public Module(string name, char type, List<string> output)
 			{
 				this.name = name;
 				this.type = type;
 				this.output = output;
 				if (type == '%')
 					state = false;
-				if(type == '&' || type == 'N')
+				if (type == '&' || type == 'N')
 					state = new InputMemory();
 			}
 			public void AddInputModules(string[] input)
 			{
-				foreach(var line in input)
+				foreach (var line in input)
 					if (line.Split("->")[1].Contains(name))
 						(state as InputMemory).Add(line.Split(' ')[0].Trim(['%', '&']), false);
 			}
-            public override string ToString()
+			public override string ToString()
 			{
 				string str = type + name + ": ";
-				foreach(var module in output) 
+				foreach (var module in output)
 					str += module.ToString() + ", ";
 				return str.TrimEnd([',', ' ']);
 			}
-        }
+		}
 		public static Dictionary<string, Module> ParseModules(string[] input)
 		{
 			var dict = new Dictionary<string, Module>();
-			foreach(var line in input)
+			foreach (var line in input)
 			{
 				var split = line.Split(' ');
 				var name = split[0].Trim(['%', '&']);
-				
+
 				var modules = new List<string>();
 				for (int i = 2; i < split.Length; i++)
 					modules.Add(split[i].Trim(','));
@@ -68,14 +68,14 @@ namespace AoC2023
 		}
 		public static void QueueNewPulses(Dictionary<string, Module> modules, Queue<(Module module, string inputName, bool isHighPulse)> queue, Module curr, bool output)
 		{
-            foreach (var name in curr.output)
+			foreach (var name in curr.output)
 			{
 				if (!modules.ContainsKey(name))
 					modules.Add(name, new Module(name, 'N', new List<string>()));
-                queue.Enqueue((modules[name], curr.name, output));
-            }
-        }
-        public static void PrintModuleState(Dictionary<string, Module> modules)
+				queue.Enqueue((modules[name], curr.name, output));
+			}
+		}
+		public static void PrintModuleState(Dictionary<string, Module> modules)
 		{
 			foreach (var module in modules.Values)
 			{
@@ -90,41 +90,41 @@ namespace AoC2023
 		}
 		public static (int low, int high) PressButton(Dictionary<string, Module> modules, InputMemory lastConMemory = null, int press = 0, long[] cycles = null)
 		{
-            // button sends low pulse to broadcaster
-            int lowPulses = 1;
+			// button sends low pulse to broadcaster
+			int lowPulses = 1;
 			int highPulses = 0;
-            var queue = new Queue<(Module module, string inputName, bool isHighPulse)>();
+			var queue = new Queue<(Module module, string inputName, bool isHighPulse)>();
 
-            foreach (var name in modules["broadcaster"].output)
-                queue.Enqueue((modules[name], "broadcaster", false));
+			foreach (var name in modules["broadcaster"].output)
+				queue.Enqueue((modules[name], "broadcaster", false));
 
-            while (queue.Count > 0)
-            {
-                var deq = queue.Dequeue();
-                var curr = deq.module;
-                var inputName = deq.inputName;
-                bool isHighPulse = deq.isHighPulse;
+			while (queue.Count > 0)
+			{
+				var deq = queue.Dequeue();
+				var curr = deq.module;
+				var inputName = deq.inputName;
+				bool isHighPulse = deq.isHighPulse;
 
-                if (curr.type == '%' && !isHighPulse)
-                {
-                    curr.state = !(bool)curr.state;
-                    QueueNewPulses(modules, queue, curr, (bool)curr.state);
-                }
-                if (curr.type == '&')
-                {
-                    (curr.state as InputMemory)[inputName] = isHighPulse;
-                    bool output = (curr.state as InputMemory).Any(kvp => kvp.Value == false);
-                    QueueNewPulses(modules, queue, curr, output);
-                }
+				if (curr.type == '%' && !isHighPulse)
+				{
+					curr.state = !(bool)curr.state;
+					QueueNewPulses(modules, queue, curr, (bool)curr.state);
+				}
+				if (curr.type == '&')
+				{
+					(curr.state as InputMemory)[inputName] = isHighPulse;
+					bool output = (curr.state as InputMemory).Any(kvp => kvp.Value == false);
+					QueueNewPulses(modules, queue, curr, output);
+				}
 
-                if (isHighPulse)
-                    highPulses++;
-                else
-                    lowPulses++;
+				if (isHighPulse)
+					highPulses++;
+				else
+					lowPulses++;
 
-                // part 2
-                // pulse sent to rx -> check predefined memory for logic match (all high pulse) to get low pulse to rx 
-                if (lastConMemory != null && curr.name == "rx")
+				// part 2
+				// pulse sent to rx -> check predefined memory for logic match (all high pulse) to get low pulse to rx 
+				if (lastConMemory != null && curr.name == "rx")
 				{
 					var memoryList = lastConMemory.ToList();
 					for (int i = 0; i < lastConMemory.Count; i++)
@@ -137,26 +137,26 @@ namespace AoC2023
 				}
 			}
 			return (lowPulses, highPulses);
-        }
+		}
 		public static int Part1(string[] input)
 		{
 			var modules = ParseModules(input);
 			int lowPulses = 0;
 			int highPulses = 0;
-			
+
 			for (int i = 0; i < 1000; i++)
 			{
 				var pulses = PressButton(modules);
 				lowPulses += pulses.low;
 				highPulses += pulses.high;
-            }
-			
+			}
+
 			return lowPulses * highPulses;
 		}
 		public static long Part2(string[] input)
 		{
 			var modules = ParseModules(input);
-			
+
 			// add rx module
 			var rx = new Module("rx", 'N', new List<string>());
 			rx.AddInputModules(input);
@@ -165,14 +165,14 @@ namespace AoC2023
 			// get con modules 2 steps backwards and create cycle array
 			// this only works because these modules are all con modules
 			var rxMem = rx.state as InputMemory;
-            var lastCon = modules[rxMem.ToList()[0].Key].state as InputMemory;
+			var lastCon = modules[rxMem.ToList()[0].Key].state as InputMemory;
 			var cycles = new long[lastCon.Count];
-			
+
 			// press button as long as there is an undefined cycle
 			for (int i = 1; cycles.Any(num => num == 0); i++)
 				PressButton(modules, lastCon, i, cycles);
 
-            return LeastCommonMultiple(cycles);
+			return LeastCommonMultiple(cycles);
 		}
 	}
 }
