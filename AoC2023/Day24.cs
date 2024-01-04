6ee2931448ce8d80d0283ded1e2e89b8bf7cbfb2
NameAcht <appeljohannes21@gmail.com>
2024-01-04 14:09:19 +0100

    Update Day24.cs

--- AoC2023/Day24.cs
+++ AoC2023/Day24.cs
@@ -1,34 +1,94 @@
 using XY = System.Numerics.Vector2;
 using XYZ = System.Numerics.Vector3;
 
+//  Let:
+//    <p_rock>(t) = <X,Y,Z> + t*<DX,DY,DZ> 
+//    <p_hail>(t) = <x,y,z> + t*<dx,dy,dz>
+
+//  A rock-hail collision requires the following to be true:
+//    X+t*DX = x+t*dx
+//    Y+t*DY = y+t*dy
+//    Z+t*DZ = z+t*dz
 /*
- * 
+ *  ->
+ *  t = (X-x)/(dx-DX)
+ *  t = (Y-y)/(dy-DY)
+ *  t = (Z-z)/(dz-DZ)
+ *  
+ *  Equating the first two equalities from above yields:
+ *    (X-x)/(dx-DX) = (Y-y)/(dy-DY)
+ *    (X-x) (dy-DY) = (Y-y) (dx-DX)
+ *    X*dy - X*DY - x*dy + x*DY = Y*dx - Y*DX - y*dx + y*DX
+ *    Y*DX - X*DY = Y*dx - y*dx + y*DX - X*dy + x*dy - x*DY
+ *      
+ *  Note that the LHS of the above equation is true for any hail stone. Evaluating
+ *  the RHS again for a different hailstone, and setting the two RHS equal, yields 
+ *  the first of the below equations:
+ *      
+ *  (dy'-dy) X + (dx-dx') Y + (y-y') DX + (x'-x) DY =  x' dy' - y' dx' - x dy + y dx
+ *  (dz'-dz) X + (dx-dx') Z + (z-z') DX + (x'-x) DZ =  x' dz' - z' dx' - x dz + z dx
+ *  (dz-dz') Y + (dy'-dy) Z + (z'-z) DY + (y-y') DZ = -y' dz' + z' dy' + y dz - z dy
+ *      
+ *  The second and third are yielded by repeating the above process with X & Z, and 
+ *  then Y & Z. This is a system of equations with 6 unknowns. Using two different
+ *  pairs of hailstones (e.g. three total hailstones) yields 6 equations with 6
+ *  unknowns, which we can now solve relatively trivially using linear algebra.
+ *  
+ *  
+ *  Ex 1:
+ *  19x, 13y, 30z @ -2dx,  1dy, -2dz
+ *  18x', 19y', 22z' @ -1dx', -1dy', -2dz'
+ *  
+ *  (dy'-dy) X + (dx-dx') Y + (y-y') DX + (x'-x) DY =  x' dy' - y' dx' - x dy + y dx
+ *  (dz'-dz) X + (dx-dx') Z + (z-z') DX + (x'-x) DZ =  x' dz' - z' dx' - x dz + z dx
+ *  (dz-dz') Y + (dy'-dy) Z + (z'-z) DY + (y-y') DZ = -y' dz' + z' dy' + y dz - z dy
+ *  
+ *  (-1-1) X + (-2-(+1)) Y + (13-(+19)) DX + (18-19) DY = 18*(-1) - 19*(-1) - 19*1 + 13*(-2)
+ *  (-1-1) X + (-2-(+1)) Z + (30-22) DX + (18-19) DZ = 18*(-2) - 22*(-1) - 19*(-2) + 30*(-2)
+ *  (-2-(-2)) Y + (-1-(-1)) Z + (22-30) DY + (13-19) DZ = -19*(-2) + 22*(-1) + 13*(-2) - 30*1
+ *  
+ *  20x, 25y, 34z @ -2dx, -2dy, -4dz
+ *  12x', 31y', 28z' @ -1dx', -2dy', -1dz'
+ *  
+ *  
+ *  
+ *  
  */
 
 namespace AoC2023
 {
+    struct Vec3
+    {
+        decimal X, Y, Z;
+    }
 	struct Hailstone
 	{
-		public float px, py, pz, vx, vy, vz;
+		public float x, y, z, dx, dy, dz;
         public Hailstone EndOfLine((float min, float max) area)
         {
-            float xMov = Math.Abs(vx < 0 ? (px - area.min) / vx : (area.max - px) / vx);
-            float yMov = Math.Abs(vy < 0 ? (py - area.min) / vy : (area.max - py) / vy);
+            float xMov = Math.Abs(dx < 0 ? (x - area.min) / dx : (area.max - x) / dx);
+            float yMov = Math.Abs(dy < 0 ? (y - area.min) / dy : (area.max - y) / dy);
             float mov = Math.Min(xMov, yMov);
             var hs = new Hailstone()
             {
-                px = px + vx * mov,
-                py = py + vy * mov,
-                pz = pz + vz * mov,
-                vx = vx,
-                vy = vy,
-                vz = vz
+                x = x + dx * mov,
+                y = y + dy * mov,
+                z = z + dz * mov,
+                dx = dx,
+                dy = dy,
+                dz = dz
             };
             return hs;
         }
         public override string ToString()
         {
-            return px + ", " + py + ", " + pz + " @ " + vx + ", " + vy + ", " + vz;
+            return x + ", " + y + ", " + z + " @ " + dx + ", " + dy + ", " + dz;
+        }
+        public void PrintEquation(Hailstone p)
+        {
+            Console.WriteLine($"({p.dy}-{dy}) X + ({dx}-{p.dx}) Y + ({y} - {p.y}) DX + ({p.x} - {x}) DY = {p.x}*{p.dy} - {p.y}*{p.dx} - {x}*{dy} + {y}*{dx}");
+            Console.WriteLine($"({p.dz}-{dz}) X + ({dx}-{p.dx}) Z + ({z} - {p.z}) DX + ({p.x} - {x}) DZ = {p.x}*{p.dz} - {p.z}*{p.dx} - {x}*{dz} + {z}*{dx}");
+            Console.WriteLine($"({dz} - {p.dz}) Y + ({p.dy} - {dy}) Z + ({p.z}-{z}) DY + ({y} - {p.y}) DZ = -{p.y}*{p.dz} + {p.z}*{p.dy} + {y}*{dz} - {z}*{dy}");
         }
     }
 	internal class Day24
@@ -65,15 +125,15 @@ namespace AoC2023
             var hailstones = new List<Hailstone>();
             foreach (var line in input)
             {
-                var split = line.Split(['@', ',']);
+                var split = line.Split(new char[] { '@', ',' });
                 hailstones.Add(new Hailstone()
                 {
-                    px = float.Parse(split[0].Trim()),
-                    py = float.Parse(split[1].Trim()),
-                    pz = float.Parse(split[2].Trim()),
-                    vx = float.Parse(split[3].Trim()),
-                    vy = float.Parse(split[4].Trim()),
-                    vz = float.Parse(split[5].Trim())
+                    x = float.Parse(split[0].Trim()),
+                    y = float.Parse(split[1].Trim()),
+                    z = float.Parse(split[2].Trim()),
+                    dx = float.Parse(split[3].Trim()),
+                    dy = float.Parse(split[4].Trim()),
+                    dz = float.Parse(split[5].Trim())
                 });
             }
             return hailstones;
@@ -94,10 +154,10 @@ namespace AoC2023
                     var hs2 = hailstones[j];
                     var hs2End = hs2.EndOfLine(area);
 
-                    var p1 = new XY(hs1.px, hs1.py);
-                    var p2 = new XY(hs1End.px, hs1End.py);
-                    var p3 = new XY(hs2.px, hs2.py);
-                    var p4 = new XY(hs2End.px, hs2End.py);
+                    var p1 = new XY(hs1.x, hs1.y);
+                    var p2 = new XY(hs1End.x, hs1End.y);
+                    var p3 = new XY(hs2.x, hs2.y);
+                    var p4 = new XY(hs2End.x, hs2End.y);
 
                     if (IntersectIn2DArea(p1, p2, p3, p4, area))
                         sum++;
@@ -110,49 +170,6 @@ namespace AoC2023
         {
             var hailstones = ParseHailstones(input);
 
-            for (int i = 0; i < hailstones.Count; i++)
-            {
-                var hs1 = hailstones[i];
-                var hs1End = hs1.EndOfLine((hs1.px * (-2), hs1.px * 2));
-                //Console.WriteLine("[[" + hs1.px + "," + hs1.py + "," + hs1.pz + "],[" + hs1End.px + "," + hs1End.py + "," + hs1End.pz + "]]");
-
-                for (int j = i + 1; j < hailstones.Count; j++)
-                {
-                    var hs2 = hailstones[j];
-                    var hs2End = hs2.EndOfLine((hs2.px * (-2), hs2.px * 2));
-
-                    var p1 = new XYZ(hs1.px, hs1.py, hs1.pz);
-                    var p2 = new XYZ(hs1End.px, hs1End.py, hs1End.pz);
-                    var p3 = new XYZ(hs2.px, hs2.py, hs2.pz);
-                    var p4 = new XYZ(hs2End.px, hs2End.py, hs2End.pz);
-
-                    var p13 = p1 - p3;
-                    var p21 = p2 - p1;
-                    var p43 = p4 - p3;
-
-                    double d1343 = p13.X * (double)p43.X + (double)p13.Y * p43.Y + (double)p13.Z * p43.Z;
-                    double d4321 = p43.X * (double)p21.X + (double)p43.Y * p21.Y + (double)p43.Z * p21.Z;
-                    double d1321 = p13.X * (double)p21.X + (double)p13.Y * p21.Y + (double)p13.Z * p21.Z;
-                    double d4343 = p43.X * (double)p43.X + (double)p43.Y * p43.Y + (double)p43.Z * p43.Z;
-                    double d2121 = p21.X * (double)p21.X + (double)p21.Y * p21.Y + (double)p21.Z * p21.Z;
-
-                    double denom = d2121 * d4343 - d4321 * d4321;
-                    double numer = d1343 * d4321 - d1321 * d4343;
-
-                    double mua = numer / denom;
-                    double mub = (d1343 + d4321 * (mua)) / d4343;
-
-                    // shortest line between any two Vec3 points
-                    var resultA = new XYZ(
-                        p1.X + (float)mua * p21.X,
-                        p1.Y + (float)mua * p21.Y,
-                        p1.Z + (float)mua * p21.Z);
-                    var resultB = new XYZ(
-                        p3.X + (float)mub * p43.X,
-                        p3.Y + (float)mub * p43.Y,
-                        p3.Z + (float)mub * p43.Z);
-                }
-            }
 
             return 0;
         }
